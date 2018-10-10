aa7a35e458b1c0cd5f2caa6bea5062e30b1b32fd
developer <boris-plus@mail.ru>
2018-10-11 00:16:16 +0300

    Исчезает кнопка

--- project/static/js/bug_report.js
+++ project/static/js/bug_report.js
@@ -1,7 +1,17 @@
 class BugReport {
-    static setMessage(text){
-        var bugReportMessageArea = document.getElementById("bug_report_message_area");
-        bugReportMessageArea.innerHTML = text;
+    static setMessage(text, style){
+        var bugReportTextArea = document.getElementById("bug_report_text_area");
+        var bugReportButtonArea = document.getElementById("bug_report_button_area");
+        var re_text = bugReportTextArea.innerHTML;
+        bugReportTextArea.innerHTML = text;
+        if (style == 'normal'){
+            bugReportButtonArea.style.display = 'block';
+            bugReportTextArea.className = "bug_report_text_normal";
+        } else {
+            bugReportButtonArea.style.display = 'none';
+            bugReportTextArea.className = "bug_report_text_thanks";
+            setTimeout(() => BugReport.setMessage(re_text, 'normal'), 3000);
+        }
     }
     static getReport(event){
         var selectedText = window.getSelection().toString();
@@ -10,24 +20,23 @@ class BugReport {
                           '\n\n ' +
                           selectedText +
                           '\n\n ' +
-                          'Поясните дополнительно и укажите контактные данные, если хотите.'
-            var prompted = prompt(message, "");
-            if (prompted != null) {
-                BugReport.setMessage('Благодарю Вас!')
-                // honest send: message + user_message
-                BugReport.sendReport(message + user_message, true)
+                          'Поясните дополнительно и укажите контактные данные, если хотите.';
+            var user_prompt = prompt(message, "");
+            if (user_prompt != null) {
+                BugReport.setMessage('Благодарю Вас!');
+                // honest send: message + user_prompt
+                BugReport.sendReport(message + ':' + user_prompt, true);
             } else {
                 // force send of uncertain user unprompted message : message + user_message
-                BugReport.sendReport(message + user_message, false)
+                BugReport.sendReport(message, false);
             }
         }
 
     }
     static sendReport(text, honestMarker){
         // send realization
-        // alert(text)
+        // alert('SEND: ' + text);
     }
-
 }
 window.onload = function() {
     var button = document.getElementById("bug_report_button");
