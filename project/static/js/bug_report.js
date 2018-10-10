f3babca233a0f38e7559d7dc7289e19b45df5bd0
developer <boris-plus@mail.ru>
2018-10-11 00:53:21 +0300

    if !(selectedText){    return }

--- project/static/js/bug_report.js
+++ project/static/js/bug_report.js
@@ -15,23 +15,23 @@ class BugReport {
     }
     static getReport(event){
         var selectedText = window.getSelection().toString();
-        if (selectedText){
-            var message = window.location.href +
-                          '\n\n ' +
-                          selectedText +
-                          '\n\n ' +
-                          'Поясните дополнительно и укажите контактные данные, если хотите.';
-            var user_prompt = prompt(message, "");
-            if (user_prompt != null) {
-                BugReport.setMessage('Благодарю Вас!');
-                // honest send: message + user_prompt
-                BugReport.sendReport(message + ':' + user_prompt, true);
-            } else {
-                // force send of uncertain user unprompted message : message + user_message
-                BugReport.sendReport(message, false);
-            }
+        if !(selectedText){
+            return
         }
-
+        var message = window.location.href +
+                      '\n\n ' +
+                      selectedText +
+                      '\n\n ' +
+                      'Поясните дополнительно и укажите контактные данные, если хотите.';
+        var user_prompt = prompt(message, "");
+        if (user_prompt != null) {
+            BugReport.setMessage('Благодарю Вас!');
+            // honest send: message + user_prompt
+            BugReport.sendReport(message + ':' + user_prompt, true);
+        } else {
+            // force send of uncertain user unprompted message : message + user_message
+            BugReport.sendReport(message, false);
+        }
     }
     static sendReport(text, honestMarker){
         // send realization
