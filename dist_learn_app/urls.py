fa76ccfaea4cf7a235fb547ee6a149a4d9984359
developer <boris-plus@mail.ru>
2019-01-19 22:43:46 +0300

    changelog: * визуально разделил url на простые и пользовательские

--- dist_learn_app/urls.py
+++ dist_learn_app/urls.py
@@ -1,15 +1,17 @@
 from django.conf.urls import url
-from . import views
-from django.views.generic import TemplateView, RedirectView
+from . import views, user_views
+from django.views.generic import TemplateView
 
 urlpatterns = [
     url(r'^index$', TemplateView.as_view(template_name="dist_learn_app/pages/index.html"), name="index"),
     url(r'^contacts$', TemplateView.as_view(template_name="dist_learn_app/pages/contacts.html"), name="contacts"),
-    url(r'^login$', views.SignInOrSignUp.as_view(), name="sign_in"),
-    url(r'^logout$', views.sign_out, name="logout"),
     url(r'^course/list$', views.CourseList.as_view(), name="course_list"),
-    url(r'^subscriber/lessons$', views.SubscriberLessons.as_view(), name="subscriber_lessons"),
     url(r'^course/(?P<pk>\d+)/lessons$', views.CourseLessons.as_view(), name="course_lessons"),
-    url(r'^lesson/(?P<lesson_pk>\d+)/subscribe/change$', views.ChangeSubscribe.as_view(), name="change_subscribe"),
-    url(r'', RedirectView.as_view(url='index'), name="default")
+
+    url(r'^login$', user_views.SignInOrSignUp.as_view(), name="sign_in"),
+    url(r'^logout$', user_views.sign_out, name="logout"),
+    url(r'^subscriber/lessons$', user_views.SubscriberLessons.as_view(), name="subscriber_lessons"),
+    url(r'^lesson/(?P<lesson_pk>\d+)/subscribe/change$', user_views.ChangeSubscribe.as_view(), name="change_subscribe"),
+
+    url(r'', views.get_default_view, name="default")
 ]
